0b466feea399bda8917ba4625af353248e1dce79
DansuraitoAhiru <vqviet2007@gmail.com>
2026-04-16 17:16:15 +0700

    Add files via upload

---
+++ cau2.sql
@@ -0,0 +1,19 @@
+CREATE TABLE SHIPPERS (
+	ShipperID INT PRIMARY KEY auto_increment,  -- em ko dùng đc IDENTITY(1,1)
+    ShipperName VARCHAR(255),
+    Phone VARCHAR(20)
+);
+
+-- INSERT INTO SHIPPERS (ShipperName, Phone)
+-- VALUES ('Giao Hàng Nhanh, '0901234567');    thiếu dấu nháy đơn đóng lại chuỗi Giao Hàng Nhanh
+
+-- Sửa lại 
+INSERT INTO SHIPPERS (ShipperName, Phone)
+VALUES ('Giao Hàng Nhanh', '0901234567');
+
+-- INSERT INTO SHIPPERS
+-- VALUES ('Viettel Post');    -- ko truyền dữ liệu cho cột Phone thì chắc chắc nó sẽ NULL rồimport table from
+
+-- Sửa lại
+INSERT INTO SHIPPERS
+VALUES ('Viettel Post', '093822722722');
